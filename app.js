e4b9ca7aabc4eb0742bd0067d1d41a5244ee7bf7
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-05 17:24:05 -0600

    button

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-// Import necessary modules
-const express = require('express');
-const path = require('path');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// Create an instance of the express app
-const app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// Set the port number
-const port = process.env.PORT || 3000;
+var app = express();
 
-// Set the view engine to Pug
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// Set the views directory to where your pug templates are stored
-app.set('views', path.join(__dirname, 'views'));
-
-// Serve static files (CSS, images, JavaScript) from the 'public' directory
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route for the home page (or index page)
-app.get('/', (req, res) => {
-    res.send('Welcome to the Vehicle app!');
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-// Route for '/vehicles' to render the vehicles.pug template
-app.get('/vehicles', (req, res) => {
-    // Render 'vehicles.pug' and pass any data if necessary (for now, no extra data passed)
-    res.render('vehicles');
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports = app;
