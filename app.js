0b900ac6edaf39086d8956dac42dc5989dd1d243
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-05 19:20:32 -0600

    vehicles

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var vehiclesRouter = require('./routes/vehicles'); // Import the vehicles router
 
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -19,21 +18,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Register the routers
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/vehicles', vehiclesRouter); // Register the vehicles route
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.status(404).render('error', { message: 'Page not found', error: {} });
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/vehicles.js
+++ routes/vehicles.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('vehicles', { title: 'Search Results - Vehicles' });
+// Route for the Vehicles page
+router.get('/vehicles', (req, res) => {
+  // Array of vehicles to display
+  let results = [
+    { vehicle_name: 'Sedan', vehicle_type: 'Car', max_speed: 180 },
+    { vehicle_name: 'Sport Bike', vehicle_type: 'Motorcycle', max_speed: 220 },
+    { vehicle_name: 'SUV', vehicle_type: 'Car', max_speed: 160 }
+  ];
+
+  // Render the vehicles.pug template and pass the data
+  res.render('vehicles', { title: 'Search Results - Vehicles', results: results });
 });
 
 module.exports = router;
