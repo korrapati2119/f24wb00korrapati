6f71e648b22b29996fae75915fdf1194fda92ffc
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-05 20:19:50 -0600

    vehicles

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
+var createError = require('http-errors'); 
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var vehiclesRouter = require('./routes/vehicles'); // Import the vehicles router
+var usersRouter = require('./routes/users');
+var vehiclesRouter = require('./routes/vehicles'); // Import the vehicle router
 
 var app = express();
-
-// View engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Register the routers
 app.use('/', indexRouter);
-app.use('/vehicles', vehiclesRouter); // Register the vehicles route
-
-// Catch 404 and forward to error handler
+app.use('/users', usersRouter);
+app.use('/vehicles', vehiclesRouter); // Add the route for vehicles
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.status(404).render('error', { message: 'Page not found', error: {} });
+  next(createError(404));
 });
-
-// Error handler
+// error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
--- routes/vehicles.js
+++ routes/vehicles.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-// Route for the Vehicles page
-router.get('/vehicles', (req, res) => {
-  // Array of vehicles to display
-  let results = [
-    { vehicle_name: 'Sedan', vehicle_type: 'Car', max_speed: 180 },
-    { vehicle_name: 'Sport Bike', vehicle_type: 'Motorcycle', max_speed: 220 },
-    { vehicle_name: 'SUV', vehicle_type: 'Car', max_speed: 160 }
-  ];
+const results = [
+  { vehicles_name: 'Sedan', vehicle_type: 'Car', max_speed: 180 },
+  { vehicles_name: 'Sport Bike', vehicle_type: 'Motorcycle', max_speed: 220},
+  { vehicles_name: 'SUV', vehicle_type: 'Car', max_speed: 160}
+];
 
-  // Render the vehicles.pug template and pass the data
-  res.render('vehicles', { title: 'Search Results - Vehicles', results: results });
+// Route to render the Sculptures page
+router.get('/', (req, res) => {
+  res.render('Vehicles', { 
+    title: 'Vehicles search results', 
+    results: results  
+  });
 });
-
 module.exports = router;
