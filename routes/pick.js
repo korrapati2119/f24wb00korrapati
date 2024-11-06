d73c8b90fd91251a46bce35af190a11aae856782
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-05 21:11:55 -0600

    pics

--- routes/pick.js
+++ routes/pick.js
@@ -4,11 +4,11 @@ const router = express.Router();
 router.get('/', (req, res) => {
   // Array of image file names
   const image_names = [
-    'bob.jpeg', 
-    'balayya.jpeg', 
-    'ntr.jpeg', 
-    'god style.jpeg', 
-    'nwmsu.jpeg'
+    'bob.jpg', 
+    'balayya.jpg', 
+    'ntr.jpg', 
+    'god style.jpg', 
+    'nwmsu.jpg'
   ];
 
   res.render('randomitem', { title: 'A random item', image_names: image_names });
