13a2738ea8602bad30622f746699a58ef9354528
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-11-05 20:36:57 -0600

    grid

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/vehicles', vehiclesRouter); // Add the route for vehicles
-app.use('/grid', gridRouter); 
+app.use('/grid', gridRouter);
+ 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
